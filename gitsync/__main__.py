6687cc5b614aedbc8aa6e10c9848c502135be012
Etienne Dijon <edijon@rpb-tech.com>
2021-03-21 14:49:41 +0100

    fix: module gitsync executable

---
+++ gitsync/__main__.py
@@ -0,0 +1,8 @@
+from gitsync import gitsync
+
+if __name__ == "__main__":
+    try:
+        gitsync.main()
+    except NotImplementedError:
+        sys.argv = [sys.argv[0], "-h"]
+        gitsync.main()
