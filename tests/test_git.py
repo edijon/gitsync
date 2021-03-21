4c8a113a93a9c4851438d64175d2880b191f6f96
Etienne Dijon <edijon@rpb-tech.com>
2021-03-21 15:06:14 +0100

    chore(test_git): increase code coverage

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,11 +1,30 @@
 from gitsync.git import Git, GitPosix
+import pytest
+import os
 
+def test_git():
+    with pytest.raises(NotImplementedError):
+        os.name = "none"
+        git = get_git()
+    os.name = "posix"
 
 def get_git():
     return Git.controller("https://github.com/edijon/gitsync.git", "gitsync", "/tmp")
 
 
-def test_init():
+def test_clone():
+    with pytest.raises(NotImplementedError):
+        git = Git()
+        git.clone()
+
+
+def test_pull():
+    with pytest.raises(NotImplementedError):
+        git = Git()
+        git.pull()
+
+
+def test_posix_init():
     git = get_git()
     assert isinstance(git.url, str)
     assert isinstance(git.name, str)
@@ -14,17 +33,17 @@ def test_init():
     assert git.full_path == git.path + "/" + git.name
 
 
-def test_git_factory():
+def test_posix_git_factory():
     git = get_git()
     assert isinstance(git, GitPosix)
     assert issubclass(git.__class__, Git)
 
 
-def test_clone():
+def test_posix_clone():
     git = get_git()
     git.clone()
 
 
-def test_pull():
+def test_posix_pull():
     git = get_git()
     git.pull()
